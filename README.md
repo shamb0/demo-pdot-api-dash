01f7f1a7c0a1596485320e7326fdc8f796dfc855
shamb0 <r.raajey@gmail.com>
2020-10-28 21:11:36 +0530

    Sub017 | [BEGINNER CHALLENGES] Front-End Javascript

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# demo-pdot-api-dash
+# [BEGINNER CHALLENGES] Front-End Javascript | Polkadot-Network Funded Issue Detail | Gitcoin | Gitcoin
+
+* [[BEGINNER CHALLENGES] Front-End Javascript | Polkadot-Network Funded Issue Detail | Gitcoin | Gitcoin](https://gitcoin.co/issue/Polkadot-Network/hello-world-by-polkadot/14/100023940)
+
+![](https://i.imgur.com/Y2MgiRn.png)
+
+---
+
+## Proof of Work
+
+[Github page | Live Kusama Block Status](https://shamb0.github.io/demo-pdot-api-dash/)
+
+
+![](https://i.imgur.com/A3p73Ms.png)
+
+---
+
+## Howto
+
+* Bootstrapping
+
+```shell
+$ yarn install
+```
+
+* Development server
+
+```shell
+$ yarn run serve
+```
+
+* To deploy on Github page
+
+```shell
+$ yarn run gh-pages-deploy
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>Kusama Chain</h1>
+    <h2>Latest Block Height: {{ header.number }}</h2>
+    <h4>Latest Block Hash: {{ header.hash ? header.hash.toHex() : '' }}</h4>
+    <h4>Latest Block ParentHash: {{ header.parentHash }}</h4>
+    <h4>Latest Block Author: {{ header.author }}</h4>
+    <hr />
+    <h1>find block by height</h1>
+    <form @submit="formSubmit">
+      <input v-model="height" type="number" />
+      <button class="btn btn-success">Submit</button>
+    </form>
+    <h2>Block Height: {{ headerTarget.number }}</h2>
+    <h4>Block Hash: {{ headerTarget.hash ? headerTarget.hash.toHex() : '' }}</h4>
+    <h4>Block ParentHash: {{ headerTarget.parentHash }}</h4>
+    <h4>Block Author: {{ headerTarget.author }}</h4>
+    <a v-bind:href="url">Subscan BlockHeight {{headerTarget.number}} </a>
+    <hr />
+    <p>
+      Submitted by ...<br />
+      <a href="https://github.com/shamb0" target="_blank" rel="noopener"
+        >shamb0</a
+      >.
+    </p>
+  </div>
+</template>
+
+<script>
+import { ApiPromise, WsProvider } from "@polkadot/api";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      height: 100,
+      header: {},
+      headerTarget: {},
+      url: ' https://kusama.subscan.io/block/'
+    };
+  },
+  methods: {
+    async formSubmit(e) {
+      e.preventDefault();
+      const wsProvider = new WsProvider("wss://kusama-rpc.polkadot.io");
+      let api = await ApiPromise.create({ provider: wsProvider });
+      let hash = await api.rpc.chain.getBlockHash(this.height);
+      this.headerTarget = await api.derive.chain.getHeader(hash);
+      console.log(this.height)
+      console.log(this.headerTarget)
+      this.url = 'https://kusama.subscan.io/block/'+ this.headerTarget.number
+    },
+  },
+  async mounted() {
+    const wsProvider = new WsProvider("wss://kusama-rpc.polkadot.io");
+    let api = await ApiPromise.create({ provider: wsProvider });
+    await api.derive.chain.subscribeNewHeads((header) => {
+      console.log(header);
+      this.header = header;
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
